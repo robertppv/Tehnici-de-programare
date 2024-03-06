3dde8bc1d759ad5aede715a02dc3ba4a4d7ed518
robert <robert.popovici57@gmail.com>
2024-03-06 21:52:05 +0200

    lab3 done

--- lab3.c
+++ lab3.c
@@ -87,6 +87,7 @@ void qdisplay()
             printf("%d ", queue[i]);
         }
     }
+    printf("\n");
 }
 void qpeek()
 {
@@ -97,6 +98,98 @@ void qpeek()
     else
     {
         printf("%d", queue[front]);
+    }
+    printf("\n");
+}
+
+typedef struct
+{
+    char s[100];
+    int rand;
+} DATA;
+
+DATA dStack[MAXSIZE];
+int top2 = -1;
+
+void add(DATA item)
+{
+    if (top2 == MAXSIZE - 1)
+    {
+        printf("Stack is full!!!");
+    }
+    else
+    {
+        dStack[++top2] = item;
+    }
+}
+void undo()
+{
+    if (top2 == -1)
+    {
+        printf("Stack is empty!!!");
+    }
+    else
+    {
+        top2--;
+    }
+}
+
+void Display()
+{
+    if (top2 == -1)
+    {
+        printf("Stack is empty!!!");
+    }
+    else
+    {
+        printf("%s  %d\n", dStack[top2].s, dStack[top2].rand);
+    }
+}
+
+typedef struct
+{
+    int trans;
+    char msj[100];
+} BUF;
+
+BUF dQueue[MAXSIZE];
+int dfront = -1, drear = -1;
+
+void Add(BUF item)
+{
+    if (drear == MAXSIZE - 1)
+    {
+        printf("Queue is full!!!");
+    }
+    else
+    {
+        if (dfront == -1)
+            dfront = 0;
+        dQueue[++drear] = item;
+    }
+}
+
+void transmit()
+{
+    if (dfront == -1 || dfront > drear)
+    {
+        printf("Queue is empty!!!");
+    }
+    else
+    {
+        dfront++;
+    }
+}
+void tDisplay()
+{
+    if (dfront == -1 || dfront > drear)
+    {
+        printf("Queue is empty!!!");
+    }
+    else
+    {
+        printf("%d   %s", dQueue[dfront].trans, dQueue[dfront].msj);
+        printf("\n");
     }
 }
 int main(void)

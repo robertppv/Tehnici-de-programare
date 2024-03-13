543719bb8f53d5f1fdb2f8d0efa2d13e6c1da35d
robert <robert.popovici57@gmail.com>
2024-03-13 11:42:36 +0200

    lab4 in progress

--- lab4.c
+++ lab4.c
@@ -2,7 +2,32 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
+typedef struct
+{
+    char nume[20];
+    float nota;
+} STUDENT;
+
+void pb0()
+{
+    STUDENT stud1;
+    scanf("%19s", &stud1.nume);
+    scanf("%f", &stud1.nota);
+    printf("\n");
+    printf("%s     %f", stud1.nume, stud1.nota);
+}
+
+typedef struct
+{
+    int unsigned dim : 11;
+    
+    int nume[15];
+} FIS;
+
+void pb1()
 {
     
+}
+int main(void)
+{
 }
